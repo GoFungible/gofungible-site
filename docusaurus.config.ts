e559efdad455fe3ae9be24bc698d126ca7fbdee8
pellyadolfo <pellyadolfo@yahoo.es>
2025-11-24 18:13:05 +0100

    update baseUri

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://gofungible.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/gofungible-site/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
