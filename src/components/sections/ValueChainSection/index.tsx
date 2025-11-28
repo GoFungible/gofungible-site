d81d251a08c8ec0112912ec278178e7ed13148f0
pellyadolfo <pellyadolfo@yahoo.es>
2025-11-28 23:44:36 +0100

    update text

--- src/components/sections/ValueChainSection/index.tsx
+++ src/components/sections/ValueChainSection/index.tsx
@@ -17,9 +17,9 @@ export default function ValueChainSection(): JSX.Element {
 					<div className="col text--center padding-horiz--none">
 						<div className="card border--none shadow-none h-100 padding-horiz--none bg-transparent">
 							<img className="featureSvg__8YW mb-3" alt="Tokenization" src="img/blockchain.svg" height={100}/>
-							<h2 className={styles.clippedelement}>Tokenization</h2>
+							<h2 className={styles.clippedelement}>Configuration</h2>
 							<div className="card-body d-flex flex-column">
-								<p className="pt-3">Learn the principles and practices to capture the value of your commodity into your cryptocommodity.</p>
+								<p className="pt-3">Do not tokenize but configure. Most of patterns have already been discovered.</p>
 								<a className='btn btn-lg mt-auto' href='/docs/tokenization/intro' role='button'>Learn More</a>
 							</div>
 						</div>
@@ -41,7 +41,7 @@ export default function ValueChainSection(): JSX.Element {
 							<img className="featureSvg__8YW mb-3" alt="Trading" src="img/trading.svg" height={100}/>
 							<h2 className={styles.clippedelement}>Negotiation</h2>
 							<div className="card-body d-flex flex-column">
-								<p className="pt-3">Use the price formation mechanism of your underlying market to value your cryptocurrency.</p>
+								<p className="pt-3">Use the price formation mechanism of your underlying market to value your token.</p>
 								<a className='btn btn-lg mt-auto' href='docs/negotiation/intro' role='button'>Learn More</a>
 							</div>
 						</div>
@@ -52,7 +52,7 @@ export default function ValueChainSection(): JSX.Element {
 							<img className="featureSvg__8YW mb-3" alt="Operations" src="img/operations.svg" height={100}/>
 							<h2 className={styles.clippedelement}>Distribution</h2>
 							<div className="card-body d-flex flex-column">
-								<p className="pt-3">Choose the best techniques to distribute your cryptocommodity as the barebone for your marketplapce.</p>
+								<p className="pt-3">Choose the best techniques to distribute your token as the barebone for your marketplapce.</p>
 								<a className='btn btn-lg mt-auto' href='docs/distribution/intro' role='button'>Learn More</a>
 							</div>
 						</div>
@@ -63,7 +63,7 @@ export default function ValueChainSection(): JSX.Element {
 							<img className="featureSvg__8YW mb-3" alt="DeFi" src="img/defi.svg" height={100}/>
 							<h2 className={styles.clippedelement}>DeFi Services</h2>
 							<div className="card-body d-flex flex-column">
-								<p className="pt-3">Contribute to financial inclusion by delivering the value of your commodity back to the society.</p>
+								<p className="pt-3">Contribute to financial inclusion by delivering the value of your token back to the society.</p>
 								<a className='btn btn-lg mt-auto' href='docs/defi/intro' role='button'>Learn More</a>
 							</div>
 						</div>
@@ -74,7 +74,7 @@ export default function ValueChainSection(): JSX.Element {
 							<img className="featureSvg__8YW mb-3" alt="Crowdsolving" src="img/crowdsolving.svg" height={100}/>
 							<h2 className={styles.clippedelement}>Crowdsolving</h2>
 							<div className="card-body d-flex flex-column">
-								<p className="pt-3">Apply the framework to other commodities to enable citizens funding and solving their own problems.</p>
+								<p className="pt-3">Apply the framework to other tokens to enable citizens funding and solving their own problems.</p>
 								<a className='btn btn-lg mt-auto' href='docs/valueChain/crowdsolving/intro' role='button'>Learn More</a>
 							</div>
 						</div>
