dba962a92906aeeffbfd7e5d3b96c7809e4af482
pellyadolfo <pellyadolfo@yahoo.es>
2025-12-14 21:53:40 +0100

    updated text

--- src/components/sections/ImmutabilitySection/index.tsx
+++ src/components/sections/ImmutabilitySection/index.tsx
@@ -52,7 +52,7 @@ export default function ImmutabilitySection(): JSX.Element {
     
 			<GenericSection
 				title="Layer 4: Immutability Layer"
-				subtitle="Principles are nothing if they are not set in stone. Immutability Layer provides a persistent mechanism to host the Token Features that can be assembled in Fungible Token's Contract."
+				subtitle="Principles are nothing if they are not etched in stone. Immutability Layer provides a permanent mechanism to host the Token Features that can be assembled in Fungible Token's Contract."
 				variant="secondary"
 				padding="large"
 				headerSpacing="large"
