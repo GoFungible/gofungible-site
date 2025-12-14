f5a65934380569fff238fde07eb1a6a0211188d0
pellyadolfo <pellyadolfo@yahoo.es>
2025-12-14 17:24:13 +0100

    update text

--- docs/01 - stack/03 - principles/intro.md
+++ docs/01 - stack/03 - principles/intro.md
@@ -4,3 +4,22 @@ sidebar_position: 1
 
 # Principles Layer
 ---
+
+## I. Capture Value Faithfully
+
+
+## II. Protect Holders Value
+
+
+## III. Provide Friedly Access to Value
+
+
+## IV. Inform Token Holders
+
+
+## V. Privacy
+
+
+## VI. Compliance
+
+
--- src/components/sections/PrinciplesSection/index.tsx
+++ src/components/sections/PrinciplesSection/index.tsx
@@ -21,7 +21,7 @@ const mandatoryPrnciples: BaseCardProps[] = [
     title: "1. Capture Value Faithfully",
     description: "Tokens are not ponzi schemas. Whether captured value is speculative, reputational, utility, a price or intrinsinc (Layer 6), value must be captured into the token in an faithfull way. At any moment, token's price, must reflect market valuation of captured value.",
     buttonText: "More",
-    href: "/",
+    href: "/docs/stack/principles/intro",
   },
   {
     variant: "primary",
@@ -29,7 +29,7 @@ const mandatoryPrnciples: BaseCardProps[] = [
     title: "2. Protect Holders Value",
     description: "Token operations cannot compromise holders piece of captured value neither by manipulation of the issuer (Value Integrity) nor by interference of external agents (Token Security). The Platform must minimise the risk of Value disruption and provide recovery mechanisms.",
     buttonText: "More",
-    href: "/",
+    href: "/docs/stack/principles/intro",
   },
   {
     variant: "primary",
@@ -37,7 +37,7 @@ const mandatoryPrnciples: BaseCardProps[] = [
     title: "3. Friendly Access to Value",
     description: "Token issuer should make value operations and services friendly, which includes multichain availability, chain abstraction, usage of standards",
     buttonText: "More",
-    href: "/",
+    href: "/docs/stack/principles/intro",
   },
   {
     variant: "primary",
@@ -45,7 +45,7 @@ const mandatoryPrnciples: BaseCardProps[] = [
     title: "4. Inform Token Holders",
     description: "Token Holders must be Informed beforehand on all operations performed to the token affecting them. Investor and users need fluid and trustfull information. This includes a requirement of Transparency. We also must include configurable reports with enabled interoperability formats between servers.",
     buttonText: "More",
-    href: "/",
+    href: "/docs/stack/principles/intro",
   },
 ];
 const optionalPrnciples: BaseCardProps[] = [
@@ -55,7 +55,7 @@ const optionalPrnciples: BaseCardProps[] = [
     title: "Privacy",
     description: "The level of privacy is determined by the underlying networks where the token is deployed. This is a configuration issue for the Token Issuer. Privacy on token holders must not be in jeopardy with Transparency on token operations",
     buttonText: "More",
-    href: "/",
+    href: "/docs/stack/principles/intro",
   },
 	{
     variant: "primary",
@@ -63,7 +63,7 @@ const optionalPrnciples: BaseCardProps[] = [
     title: "Compliance",
     description: "Different juristictions apply different levels of compliance. the token issuer must have the tools, out of the box, to decide what level of compliance is suitable for his token.",
     buttonText: "More",
-    href: "/",
+    href: "/docs/stack/principles/intro",
   },
 ];
 
